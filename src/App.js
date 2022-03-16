f9e84f8b0861a71d0e9ca8a6888c3a7ef51542a8
johnisprograming <ojh3995@gmail.com>
2022-03-16 11:08:51 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Content1 from './components/Content1';
+import Content2 from './components/Content2';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onPlus = () => setCount(count + 1);
+
+  const onMinus = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+    //setCount(count > 0 ? count - 1 : count);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Content1 count={count}></Content1>
+      <Content2 onPlus={onPlus} onMinus={onMinus}></Content2>
     </div>
   );
 }
---
+++ src/components/Content1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content1 = ({ count }) => {
+    return (
+        <div className='content1'>
+            <h3>카운트 : {count} </h3>
+        </div>
+    );
+};
+
+export default Content1;
